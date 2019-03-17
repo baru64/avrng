f65862a41df2756821bef0590c932e13a85efd01
Bartosz Krakowiak <bkrakowiak@man.poznan.pl>
2019-03-17 23:28:50 +0100

    Add cli

--- avrng.py
+++ avrng.py
@@ -1,3 +1,4 @@
+import argparse
 from video import VideoHandler
 from audio import AudioHandler
 
@@ -6,12 +7,19 @@ class Avrng():
 
     def __init__(self, video_source, audio_source):
         self.video_source = VideoHandler(video_source)
-        self.audio_source = AudioHandler(audio_source)
+        self.audio_source = AudioHandler(device=int(audio_source))
 
     def get_byte(self):
         # TODO avrng algorithm
-        pass
 
 if __name__ == "__main__":
-    #TODO cli app
-    print("Not implemented yet")
+    parser = argparse.ArgumentParser(
+        description="True random number generator based on audio and video sources"
+        )
+    parser.add_argument('-a', '--audio-source', type=int, default=0)
+    parser.add_argument('-v', '--video-source')
+    parser.add_argument('length', type=int, help='Length of random bytes array')
+    args = parser.parse_args()
+
+    trng = Avrng(args.video_source, args.audio_source)
+    print([trng.get_byte() for i in range(int(args.length))])
