7f6b5e5e8374fff92d98c9319d8d018c34c8a136
Bartosz Krakowiak <bkrakowiak@man.poznan.pl>
2019-03-18 12:15:03 +0100

    Edit AudioHandler

--- audio.py
+++ audio.py
@@ -1,4 +1,5 @@
 import sounddevice as sd
+import numpy as np
 
 class AudioHandler():
 
@@ -10,13 +11,14 @@ class AudioHandler():
         self.fs = sample_rate
         self.duration = duration
 
-    def get_recording(self):
+    def get_recording(self, channel=0):
         recording = sd.rec(self.duration*self.fs, blocking=True)
-        return recording
+        array = [np.uint8((sample[channel] + 1) * 128) for sample in recording]
+        return array
 
-    def get_sample(self, index=0, channel=0):
+    def get_sample(self, index=0):
         recording = self.get_recording()
-        return int((recording[index][channel] + 1) * 128)
+        return recording[index]
 
 if __name__ == "__main__":
     audio = AudioHandler()
