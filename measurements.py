4753705d18d5729f234f8cadd2fe5e430c1cb1c0
Bartosz Krakowiak <bkrakowiak@man.poznan.pl>
2019-03-24 20:39:41 +0100

    Add zip compression

--- measurements.py
+++ measurements.py
@@ -27,23 +27,30 @@ if __name__ == "__main__":
     trng = Avrng(0,0)
     x = np.array([trng.get_byte() for i in range(SIZE)], dtype='uint8')
 
-    print("Entropia: {}".format(entropy(x)))
+    print("Entropy: {}".format(entropy(x)))
 
     plt.figure()
     histogram = hist(x)
     plt.bar(histogram.keys(), histogram.values(), width=1, color='g')
+    plt.title("Histogram of random bytes")
     plt.grid(True)
     
     plt.figure()
     x.tofile(FILENAME)
     subprocess.run(["xz", "-kf", FILENAME])
     subprocess.run(["gzip", "-kf", FILENAME])
+    if os.path.isfile(FILENAME + ".zip"):
+        os.remove(FILENAME + ".zip")
+    os.system("7z a -tzip " + FILENAME + ".zip " + FILENAME + " > /dev/null")
     
     compression_data = dict()
     compression_data['uncompressed'] = os.path.getsize(FILENAME)
     compression_data['xz (LZMA)'] = os.path.getsize(FILENAME + '.xz')
     compression_data['gzip (LZ77)'] = os.path.getsize(FILENAME + '.gz')
+    compression_data['zip'] = os.path.getsize(FILENAME + '.zip')
 
+    print(compression_data)
     plt.bar(compression_data.keys(), compression_data.values())
+    plt.title("File size")
     plt.grid(True)
     plt.show()
