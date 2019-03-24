33c6845cf305c40eda1d0127d8eb9e7247653c0f
Bartosz Krakowiak <bkrakowiak@man.poznan.pl>
2019-03-24 19:51:12 +0100

    Measurement stuff

--- measurements.py
+++ measurements.py
@@ -1,27 +1,49 @@
 import math
+import subprocess
+import os
 from avrng import Avrng
+import numpy as np
 import matplotlib.pyplot as plt
 
 def entropy(n):
     result = 0
-
-    p = dict((value, 0) for value in range(256))
-    for v in n:
-        p[v] += 1
-
-    size = len(p)
+    p = hist(n)
+    size = len(n)
     for _, i in p.items():
         if i:
             result += ((i/size)*math.log2(i/size))
     return -result
 
+def hist(n):
+    p = dict((value, 0) for value in range(256))
+    for v in n:
+        p[v] += 1
+    return p
+
+SIZE = 10000
+FILENAME = 'random.bin'
+
 if __name__ == "__main__":
     trng = Avrng(0,0)
-    x = [trng.get_byte() for i in range(10000)]
-    # entropia pijana lub niespełna rozumu
+    x = np.array([trng.get_byte() for i in range(SIZE)], dtype='uint8')
+
     print("Entropia: {}".format(entropy(x)))
-    # naprawić histogram
-    n, bins, patches = plt.hist(x, 50, density=1, facecolor='g', alpha=0.75)
+
+    plt.figure()
+    histogram = hist(x)
+    plt.bar(histogram.keys(), histogram.values(), width=1, color='g')
+    plt.grid(True)
+    
+    plt.figure()
+    x.tofile(FILENAME)
+    subprocess.run(["xz", "-kf", FILENAME])
+    subprocess.run(["gzip", "-kf", FILENAME])
+    
+    compression_data = dict()
+    compression_data['uncompressed'] = os.path.getsize(FILENAME)
+    compression_data['xz (LZMA)'] = os.path.getsize(FILENAME + '.xz')
+    compression_data['gzip (LZ77)'] = os.path.getsize(FILENAME + '.gz')
+
+    plt.bar(compression_data.keys(), compression_data.values())
     plt.grid(True)
     plt.show()
-    # TODO kompresja
